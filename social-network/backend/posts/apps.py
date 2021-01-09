fa711c9070874ff4e4a4605228e3b70b28c82fbb
Andrey Fomichev <worka.zip@gmail.com>
2021-01-10 02:48:07 +0300

    removed LikesByDate creation on app start, added migrations

--- social-network/backend/posts/apps.py
+++ social-network/backend/posts/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     name = 'posts'
 
-    def ready(self):
-        from posts.models import LikesByDay
-        from django.utils.timezone import now
-        LikesByDay.objects.get_or_create(date=now())
